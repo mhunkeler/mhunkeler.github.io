488710129c1ac137d435bd17a5aba3012f309a01
Mariano <mariano.hunkeler@rfindustrial.com>
2021-09-04 20:12:58 -0300

    segundo

---
+++ demo/demo.js
@@ -0,0 +1,14 @@
+addEventListener('DOMContentLoaded', function () {
+
+	pickmeup('#fecha-init-riego', {
+		position       : 'up',
+		hide_on_select : true
+	});
+
+	pickmeup('#fecha-end-riego', {
+		position       : 'up',
+		hide_on_select : true
+	});
+
+
+});
