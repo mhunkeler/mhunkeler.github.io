7408dfd158d67ea7a6a66654ef66fd556c4cfa48
Mariano <mariano.hunkeler@rfindustrial.com>
2021-09-04 21:51:15 -0300

    se agregan iconos

--- demo/demo.js
+++ demo/demo.js
@@ -1,14 +1,15 @@
 addEventListener('DOMContentLoaded', function () {
 
 	pickmeup('#fecha-init-riego', {
-		position       : 'up',
-		hide_on_select : true
+		flat      : true,
+		mode      : 'range',
+		calendars : 1
 	});
 
-	pickmeup('#fecha-end-riego', {
-		position       : 'up',
-		hide_on_select : true
-	});
+//	pickmeup('#fecha-end-riego', {
+//		position       : 'up',
+//		hide_on_select : true
+//	});
 
 
 });
