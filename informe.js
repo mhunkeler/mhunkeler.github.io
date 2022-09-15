0bae43b992a59e5c6bafeec80c9da9ca77d2e967
Cristian <cristianponce.unaj@gmail.com>
2022-09-15 10:00:44 -0300

    up-Contrapesos

--- informe.js
+++ informe.js
@@ -10,8 +10,14 @@ import {
     const gpmApi = document.getElementById("gpm-api");
     //CONTRAPESOS
     const tipoContrapeso1 = document.getElementById("tipo-1");
+    const cantidadContrapeso1 = document.getElementById("cantidad-1");
+    const distanciaContrapeso1 = document.getElementById("distancia-1");
+    const cantidadAuxContrapeso1 = document.getElementById("cantidadAux-1");
 
-
+    const tipoContrapeso2 = document.getElementById("tipo-2");
+    const cantidadContrapeso2 = document.getElementById("cantidad-2");
+    const distanciaContrapeso2 = document.getElementById("distancia-2");
+    const cantidadAuxContrapeso2 = document.getElementById("cantidadAux-2");
     // MOTOR
     const tipoMotor = document.getElementById("tipo-motor");
     const potenciaMotor = document.getElementById("potencia-motor");
@@ -20,17 +26,10 @@ import {
     // BOMBA DE PROFUNDIDAD
     const diametroProfundidad = document.getElementById("diametro-profundidad");
     const bombaProfundidad = document.getElementById("bomba-profundidad")
-    
     // DATOS DE PRODUCCION
     const porcentajeAgua = document.getElementById("porcentaje-agua");
-   
-  //const designacionApi = document.getElementById("designacion-api");
-  //const designacionApi = document.getElementById("designacion-api");
-  //const designacionApi = document.getElementById("designacion-api");
-  //const designacionApi = document.getElementById("designacion-api");
   //const designacionApi = document.getElementById("designacion-api");
   //const tasksContainer = document.getElementById("tasks-container"); 
-  
   
   window.addEventListener("DOMContentLoaded", async (e) => {
     let params = new URLSearchParams(location.search);
@@ -54,6 +53,14 @@ import {
     gpmApi.innerHTML = Config.UnidadBombeo.GpmApi;
     //CONTRAPESOS
     tipoContrapeso1.innerHTML = Config.Contrapesos[0];
+    cantidadContrapeso1.innerHTML = Config.Contrapesos[1];
+    distanciaContrapeso1.innerHTML = Config.Contrapesos[2];
+    cantidadAuxContrapeso1.innerHTML = Config.Contrapesos[3];
+
+    tipoContrapeso2.innerHTML = Config.Contrapesos[4];
+    cantidadContrapeso2.innerHTML = Config.Contrapesos[5];
+    distanciaContrapeso2.innerHTML = Config.Contrapesos[6];
+    cantidadAuxContrapeso2.innerHTML = Config.Contrapesos[7];
     // MOTOR
     tipoMotor.innerHTML = Config.Motor.Tipo;
     potenciaMotor.innerHTML = Config.Motor.Potencia;
